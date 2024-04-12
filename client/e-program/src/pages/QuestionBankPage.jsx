5960644942310db593a89232e1a28d9f0c40c754
kamdev choudhary <kamdev-choudhary>
2024-04-13 00:32:47 +0530

    updated

--- client/e-program/src/pages/QuestionBankPage.jsx
+++ client/e-program/src/pages/QuestionBankPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Modal, Button } from "react-bootstrap";
-import "./QuestionBankPage.css";
 import SingleCorrectQuestion from "../components/SingleCorrectQuestion";
 import MultiCorrectQuestion from "../components/MultiCorrectQuestion";
 import IntegerType from "../components/IntegerType";
