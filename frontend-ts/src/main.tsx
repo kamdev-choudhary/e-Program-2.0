113aa4e9f389bfb200fe4bded457ce8c871d6855
Kamdev choudhary <kamdevchoudhary@gmail.com>
2025-01-07 11:02:06 +0530

    updated

--- frontend-ts/src/main.tsx
+++ frontend-ts/src/main.tsx
@@ -4,17 +4,28 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { lightTheme } from "./constant/theme.ts";
+import { GlobalProvider } from "./GlobalProvider.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const isDevelopment = import.meta.env.MODE === "development";
 
 const AppWrapper = (
-  <Provider store={store}>
-    <ThemeProvider theme={lightTheme}>
-      <App />;
-    </ThemeProvider>
-  </Provider>
+  <GlobalProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <App />;
+          </LocalizationProvider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
+  </GlobalProvider>
 );
 
 createRoot(document.getElementById("root")!).render(
