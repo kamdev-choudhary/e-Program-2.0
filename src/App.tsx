62bfd5cc174b0f40c673c954ac5302f6d80867a8
kamdev-bharatai <kamdev@thebharatai.co.in>
2025-04-11 10:40:50 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes.tsx";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import "./i18";
+import "./i18.ts";
 import PopUpPages from "./layout/PopUpPages.tsx";
 
 // Create QueryClient once outside the component
